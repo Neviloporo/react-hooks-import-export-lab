d99c59aacbc14091d87480f1545080e8a440fbb5
nevil <nevil.oporo@student.moringaschool.com>
2025-04-09 18:20:08 +0300

    export-lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,10 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        Welcome Home
       </h1>
     </div>
   );
 }
+
+export default Home;
